d530da0be9f54e110beea2e43a7b77e830a376e7
牧之 <iooikaak@gmail.com>
2024-02-22 23:52:12 +0800

    fix bug

--- main.go
+++ main.go
@@ -19,9 +19,10 @@ import (
 func main() {
 	//远程读取apollo配置文件
 	//初始化apollo
-	if err := config.ApolloInit(); err != nil {
-		panic(err)
-	}
+	//if err := config.ApolloInit(); err != nil {
+	//	panic(err)
+	//}
+	config.Init()
 	tracer, closer, err := config.CreateTracer(enum.ServiceName.String())
 	defer closer.Close()
 	if err != nil {
